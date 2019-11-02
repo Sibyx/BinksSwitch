719ef19f5b3f629c5edefb37e016bd625a65577b
Jakub Dubec <jakub.dubec@gmail.com>
2019-11-02 16:17:21 +0100

    Initial 🤴🏽

---
+++ BinksSwitch/Network/NetworkSwitch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpPcap;
+
+namespace BinksSwitch.Network
+{
+    public class NetworkSwitch
+    {
+        public NetworkSwitch()
+        {
+            var devices = CaptureDeviceList.Instance;
+
+            foreach (var device in devices)
+            {
+                Console.WriteLine(device);
+            }
+        }
+    }
+}
